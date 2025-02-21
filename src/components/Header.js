7be15eca3fc2010530214365d4449fdebfc04a22
Developmenttdid <development@terra-drone.co.id>
2025-02-21 13:22:44 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import {Link} from 'react-router-dom';
+import './Header.css';
 
 function Header() {
   return (
-    <header class="p-3 mb-3 border-bottom">
-      <div class="container">
+    <header class="p-3 mb-3 border-bottom" style={{backgroundColor: '#0F0F56'}}>
+      <div class="container-fluid">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <img src={process.env.PUBLIC_URL + '/logo1.svg'} width={150} height={150} className="img-fluid me-5"/> 
           <a
             href="/"
             class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none"
@@ -16,64 +19,59 @@ function Header() {
 
           <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
-              <a href="#" class="nav-link px-2 link-secondary">
-                Overview
-              </a>
+              <Link to="/" className="nav-link px-3 link-info text-white">Homepage</Link>
             </li>
             <li>
-              <a href="#" class="nav-link px-2 link-body-emphasis">
-                Inventory
-              </a>
+              <Link to="/map" className="nav-link px-3 link-info link-offset-2 link-underline-opacity-200 link-underline-opacity-100-hover text-white">Map</Link>
             </li>
             <li>
-              <a href="#" class="nav-link px-2 link-body-emphasis">
-                Customers
-              </a>
+              <Link to="/projects" className="nav-link px-3 link-info text-white">Projects</Link>
             </li>
             <li>
-              <a href="#" class="nav-link px-2 link-body-emphasis">
-                Products
-              </a>
+              <Link to="/company" className="nav-link px-3 link-info text-white">Company</Link>
+            </li>
+            <li>
+              <Link to="/flightdb" className="nav-link px-3 link-info text-white">Flight Database</Link>
             </li>
           </ul>
 
 
-          <div class="dropdown text-end">
+          <div className="dropdown text-end text-white">
             <a
               href="#"
-              class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
+              className="d-block link-body-emphasis text-decoration-none dropdown-toggle text-white"
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
               <img
-                src="https://github.com/mdo.png"
+                src={process.env.PUBLIC_URL + '/logo1.svg'}
                 alt="mdo"
                 width="32"
                 height="32"
-                class="rounded-circle"
+                className="rounded-circle border border-2 border-white"
               />
             </a>
-            <ul class="dropdown-menu text-small">
+            <ul className="dropdown-menu text-small">
               <li>
-                <a class="dropdown-item" href="#">
+                <a className="dropdown-item" href="#">
                   New project...
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="#">
+                <a className="dropdown-item" href="#">
                   Settings
                 </a>
               </li>
               <li>
-                <a class="dropdown-item" href="#">
+                <a className="dropdown-item" href="#">
                   Profile
                 </a>
               </li>
               <li>
-                <hr class="dropdown-divider" />
+                <hr className="dropdown-divider" />
               </li>
               <li>
-                <a class="dropdown-item" href="#">
+                <a className="dropdown-item" href="#">
                   Sign out
                 </a>
               </li>
@@ -86,4 +84,5 @@ function Header() {
 }
 
 export default Header;
+
 
