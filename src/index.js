092ca73fbc02737c7c8cb5cabd4bd99a50e3de3a
Developmenttdid <development@terra-drone.co.id>
2025-02-25 15:01:38 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import './index.css';
 import App from './pages/App';
 import Homepage from './pages/Homepage';
 import Project from './pages/Project';
+import Company from './pages/Company';
 import Header from './components/Header';
+import Flightdb from './pages/Flightdb';
 import Map from './pages/Map';
+import Profile from './pages/Profile';
+import Addproject from './pages/Addproject';
 import reportWebVitals from './reportWebVitals';
+import ProfileNav from './components/profilenav';
+import ChangePassword from './pages/ChangePassword';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,7 +24,14 @@ root.render(
         <Route element={<HeaderLayout />}>
           <Route path="/Homepage" element={<Homepage />} />
           <Route path="/Project" element={<Project />} />
+          <Route path="/Company" element={<Company />} />
+          <Route path="/Flightdb" element={<Flightdb />} />
           <Route path="/Map" element={<Map />} />
+          <Route path="/Project/Addproject" element={<Addproject />} />
+          <Route element={<NavprofileLayout />}>
+            <Route path="/Profile/AccountDetails" element={<Profile />} />
+            <Route path="/Profile/ChangePassword" element={<ChangePassword />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
@@ -33,5 +46,16 @@ function HeaderLayout() {
       <Header />
       <Outlet />
     </>
+  );
+}
+
+function NavprofileLayout() {
+  return (
+    <div style={{ display: "flex" }}>
+      <ProfileNav />
+      <div style={{ flex: 1, padding: "20px" }}>
+        <Outlet />
+      </div>
+    </div>
   );
 }
