e39e189a75793419c180bbfdfcfa635d2eb53b3b
Developmenttdid <development@terra-drone.co.id>
2025-03-04 10:56:39 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
-import './index.css';
-import App from './pages/App';
-import Homepage from './pages/Homepage';
-import Project from './pages/Project';
-import Company from './pages/Company';
-import Header from './components/Header';
-import Checklistdb from './pages/Flightdatabase/Flightdb';
-import Map from './pages/Map';
-import Profile from './pages/Profile';
-import ProjectStatus from './pages/Project Config/projectstatus';
-import reportWebVitals from './reportWebVitals';
-import ProfileNav from './components/profilenav';
-import ChangePassword from './pages/ChangePassword';
-import ProjectLogbook from './pages/Flightdatabase/Projectlog';
-import UAVLogbook from './pages/Flightdatabase/Uavlog';
-import FlightRecord from './pages/Flightdatabase/Flightrec';
-import ConfigNav from './components/confignav';
-import ProjectTimeline from './pages/Project Config/projecttimeline';
-import Personnel from './pages/Project Config/personnel';
-import Equipment from './pages/Project Config/equipment';
-import LegalDocument from './pages/Project Config/legaldoc';
-import ForgotPassword from './pages/forgotpassword';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Outlet,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import DatabaseNav from "./components/databasenav";
+import Header from "./components/Header";
+import ProfileNav from "./components/profilenav";
+import "./index.css";
+import Addproject from "./pages/Addproject";
+import App from "./pages/App";
+import ChangePassword from "./pages/ChangePassword";
+import Company from "./pages/Company";
+import Documents from "./pages/Documents";
+import Equipment from "./pages/Equipment";
+import Checklistdb from "./pages/Flightdb";
+import FlightRecord from "./pages/Flightrec";
+import Homepage from "./pages/Homepage";
+import Map from "./pages/Map";
+import OperationManual from "./pages/OperationManual";
+import Personnel from "./pages/Personnel";
+import Profile from "./pages/Profile";
+import Project from "./pages/Project";
+import ProjectLogbook from "./pages/Projectlog";
+import ProjectStatus from "./pages/ProjectStatus";
+import SOP from "./pages/SOP";
+import UAVLogbook from "./pages/Uavlog";
+import reportWebVitals from "./reportWebVitals";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
@@ -34,26 +39,28 @@ root.render(
         <Route element={<HeaderLayout />}>
           <Route path="/Homepage" element={<Homepage />} />
           <Route path="/Project" element={<Project />} />
-          <Route element = {<NavconfigLayout />}>
-            <Route path="/Project/ProjectStatus" element={< ProjectStatus/>} />
-            <Route path="/Project/ProjectTimeline" element={<ProjectTimeline />} />
-            <Route path="/Project/Personnel" element={<Personnel />} />
-            <Route path="/Project/Equipment" element={<Equipment />} />
-            <Route path="/Project/LegalDocument" element={<LegalDocument />} />
-          </Route>
           <Route path="/Company" element={<Company />} />
-          <Route path="FlightDatabase" element={<Checklistdb />} />
-          <Route path="FlightDatabase/ProjectLogbook" element={<ProjectLogbook />} />
-          <Route path="FlightDatabase/UAVLogbook" element={<UAVLogbook />} />
-          <Route path="FlightDatabase/FlightRecord" element={<FlightRecord />} />
+          <Route path="/Checklistdb" element={<Checklistdb />} />
+          <Route path="/ProjectLogbook" element={<ProjectLogbook />} />
+          <Route path="/UAVLogbook" element={<UAVLogbook />} />
+          <Route path="/FlightRecord" element={<FlightRecord />} />
+          <Route path="/Documents" element={<Documents />} />
+          <Route path="/Equipment" element={<Equipment />} />
+          <Route path="/Personnel" element={<Personnel />} />
+          <Route path="/ProjectStatus" element={<ProjectStatus />} />
+          <Route path="/OperationManual" element={<OperationManual />} />
+          <Route path="/SOP" element={<SOP />} />
+
           <Route path="/Map" element={<Map />} />
-          <Route path="/Project/ProjectStatus" element={<ProjectStatus />} />
+          <Route path="/Project/Addproject" element={<Addproject />} />
           <Route element={<NavprofileLayout />}>
             <Route path="/Profile/AccountDetails" element={<Profile />} />
-            <Route path="/Profile/ChangePassword" element={<ChangePassword />} />
+            <Route
+              path="/Profile-/ChangePassword"
+              element={<ChangePassword />}
+            />
           </Route>
         </Route>
-        <Route path="/ForgotPassword" element={<ForgotPassword />} />
       </Routes>
     </Router>
   </React.StrictMode>
@@ -81,10 +88,10 @@ function NavprofileLayout() {
   );
 }
 
-function NavconfigLayout() {
+function NavdatabaseLayout() {
   return (
-    <div style={{ display: "flex"}}>
-      <ConfigNav />
+    <div style={{ display: "flex" }}>
+      <DatabaseNav />
       <div style={{ flex: 1, padding: "20px" }}>
         <Outlet />
       </div>
