b53e2a2d4181c6e09a46fba5240d468e3c8fcad1
Developmenttdid <development@terra-drone.co.id>
2025-03-06 11:49:52 +0700

    Update legaldoc.jsx

--- src/pages/Project Config/legaldoc.jsx
+++ src/pages/Project Config/legaldoc.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 function LegalDocument() {
     document.body.style.overflow = "auto";
+
+    // const [sertificateList, setSertificateList] = useState([]);
+    const [permissionList, setPermissionList] = useState([]);
+    const [otherDocumentList, setOtherDocumentList] = useState([]);
+
+    // const handleAddCertificate = () => {
+    //     setSertificateList([...sertificateList, {id: Date.now()}]);
+    // }
+
+    const handleAddPermission = () => {
+        setPermissionList([...permissionList, {id: Date.now()}]);
+    }
+
+    const handleOtherDocument = () => {
+        setOtherDocumentList([...otherDocumentList, {id: Date.now()}]);
+    }
+
   return (
     <div className="document-container" style={{ marginLeft: "250px" }}>
         <h2>Document</h2>
@@ -22,15 +39,26 @@ function LegalDocument() {
                     <td><input className="form-control mb-3" id="equipment" type="text" readOnly /></td>
                     <td><input className="form-control mb-3" id="equipment" type="text" readOnly /></td>
                     <td><input className="form-control mb-3" id="equipment" type="text" readOnly /></td>
-                    <td><button type="button" class="btn btn-primary"><i class="bi bi-download"></i></button>
-                    <button type="button" class="btn btn-warning ms-2"><i class="bi bi-eye"></i></button></td>
+                    <td>
+                    <button type="button" class="btn btn-primary"><i class="bi bi-download"></i></button>
+                    <button type="button" class="btn btn-warning ms-2"><i class="bi bi-eye"></i></button>
+                    </td>
                 </tr>
                 </tbody>
             </table>
         </div>
-        <h4>#Permission</h4>
-        <div className="col mb-3 ms-3">
-            <button type="button" className="button-add btn btn-primary mb-3 me-1"><i className="bi bi-plus"></i></button>
+        
+        <div className="col mb-3 ms-3 mt-5">
+            <div className="row">
+                <div className="col-8">
+                    <h4>#Permission</h4>
+                </div>
+                <div className="col-4">
+                    <button type="button" onClick={handleAddPermission} className="button-add btn btn-primary mb-3 me-1"><i className="bi bi-plus"></i></button>
+                </div>
+                
+                
+            </div>
             <table className="table text-center table-bordered" style={{ borderColor: '#143893' }}>
                 <thead>
                 <tr>
@@ -43,8 +71,9 @@ function LegalDocument() {
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <th scope="row">1</th>
+                {permissionList.map((item, index) => (
+                    <tr key={item.id}>
+                    <th scope="row">{index + 1}</th>
                     <td><input className="form-control mb-3" id="equipment" type="file" readOnly /></td>
                     <td><input className="form-control mb-3" id="equipment" type="text" placeholder="Type document name"/></td>
                     <td><input className="form-control mb-3" id="equipment" type="text" placeholder="Type document ID"/></td>
@@ -52,12 +81,21 @@ function LegalDocument() {
                     <td><button type="button" class="btn btn-primary"><i class="bi bi-download"></i></button>
                     <button type="button" class="btn btn-warning ms-2"><i class="bi bi-eye"></i></button></td>
                 </tr>
+                ))}
                 </tbody>
             </table>
         </div>
-        <h4>#Other Document</h4>
-        <div className="col mb-3 ms-3">
-            <button type="button" className="button-add btn btn-primary mb-3 me-1"><i class="bi bi-plus"></i></button>
+       
+        <div className="col mb-3 ms-3 mt-5">
+            <div className="row">
+                <div className="col-8">
+                    <h4 className="">#Other Document</h4>
+                </div>
+                <div className="col-4">
+                    <button type="button" onClick={handleOtherDocument} className="button-add btn btn-primary mb-3 me-1"><i class="bi bi-plus"></i></button>
+                </div>
+            </div>
+            
             <table className="table text-center table-bordered" style={{ borderColor: '#143893' }}>
                 <thead>
                 <tr>
@@ -70,20 +108,36 @@ function LegalDocument() {
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <th scope="row">1</th>
+                {otherDocumentList.map((item, index) => (
+                    <tr key={item.id}>
+                    <th scope="row">{index + 1}</th>
                     <td><input className="form-control mb-3" id="equipment" type="file" readOnly /></td>
                     <td><input className="form-control mb-3" id="equipment" type="text" placeholder="Type document name"/></td>
                     <td><input className="form-control mb-3" id="equipment" type="text" placeholder="Type document ID"/></td>
                     <td><input className="form-control mb-3" id="equipment" type="date" /></td>
-                    <td><button type="button" class="btn btn-primary"><i class="bi bi-download"></i></button>
-                    <button type="button" class="btn btn-warning ms-2"><i class="bi bi-eye"></i></button></td>
+                    <td>
+                        <div className="container-fluid text-center">
+                            <div className="row">
+                                <div className="col-12">
+                                    <button type="button" class="btn btn-primary"><i class="bi bi-download"></i></button>
+                                </div>
+                                <div className="col-12">
+                                    <button type="button" class="btn btn-warning ms-2"><i class="bi bi-eye"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    
+                    </td>
                 </tr>
+                ))}
                 </tbody>
             </table>
         </div>
     </div>
+
   );
 }
 
 export default LegalDocument;
+
