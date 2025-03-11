6dc383cf5a9e6d970931e342a630ecd82dd65287
Developmenttdid <development@terra-drone.co.id>
2025-03-11 13:53:11 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Profile.css";
 
 function Profile() {
+    const email = localStorage.getItem("email");
+    const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://103.163.184.111:3000/users`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data.find((u) => u.email === email) || null);
+      })
+      .catch((err) => console.error("Error fetching user data:", err));
+  }, [email]);
+
+  if (!user) return <p>Loading user data...</p>;
+  
+
   return (
     <div className="profile-details ms-0 p-2">
         <h2 className="profile-container mb-4">Account Details</h2>
         <div className="col mb-3 ms-3">
             <label className="form-label">Name</label>
-            <input type="text" className="form-control" value="John Doe" readOnly />
+            <input type="text" className="form-control" value={user.name} readOnly />
         </div>
         <div className="col mb-3 ms-3">
             <label className="form-label">Email Address</label>
-            <input type="text" className="form-control" value="johndoe@example.com" readOnly />
+            <input type="text" className="form-control" value={email} readOnly />
         </div>
         <div className="col mb-3 ms-3">
             <label className="form-label">Department</label>
@@ -23,10 +38,11 @@ function Profile() {
         </div>
         <div className="col mb-3 ms-3">
             <label className="form-label">User Type</label>
-            <input type="text" className="form-control" value="Admin" readOnly />
+            <input type="text" className="form-control" value={user.role} readOnly />
         </div>
     </div>
   );
 }
 
 export default Profile;
+
