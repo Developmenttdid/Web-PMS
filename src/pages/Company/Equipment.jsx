ecb1d9ae43eb2eead9af4fd889ba5212e476fdf2
Developmenttdid <development@terra-drone.co.id>
2025-03-12 14:11:27 +0700

    Update Equipment.jsx

--- src/pages/Company/Equipment.jsx
+++ src/pages/Company/Equipment.jsx
@@ -1,10 +1,27 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function EquipmentCompany() {
+  document.body.style.overflowY = "auto";
+  document.body.style.overflowX = "hidden";
   const [selectedTable, setSelectedTable] = useState("UAV");
+  const [equipmentData, setEquipmentData] = useState([]);
+
+  useEffect(() => {
+    fetchEquipmentData();
+  }, [selectedTable]);
+
+  const fetchEquipmentData = async () => {
+    try {
+      const response = await axios.get("http://103.163.184.111:3000/equipment_database");
+      setEquipmentData(response.data);
+    } catch (error) {
+      console.error("Error fetching equipment data:", error);
+    }
+  };
 
   return (
     <div>
@@ -15,13 +32,13 @@ function EquipmentCompany() {
             <select
               className="form-select"
               value={selectedTable}
-              onChange={(e) => {
-                setSelectedTable(e.target.value);
-              }}
+              onChange={(e) => setSelectedTable(e.target.value)}
             >
               <option value="UAV">UAV</option>
               <option value="Payload">Payload</option>
               <option value="Geodetic GPS">GPS</option>
+              <option value="Laptop">Laptop</option>
+              <option value="Battery">Battery</option>
               <option value="Other">Other</option>
             </select>
           </div>
@@ -31,10 +48,12 @@ function EquipmentCompany() {
       <div className="container-fluid mt-5">
         <div className="row">
           <div className="col-12">
-            {/* Menampilkan tabel berdasarkan pilihan */}
-            {selectedTable === "UAV" && <UAVTable />}
-            {selectedTable === "Payload" && <PayloadTable />}
-            {selectedTable === "Geodetic GPS" && <GeodeticGPSTable />}
+            {selectedTable === "UAV" && <EquipmentTable data={equipmentData} type="drone" />}
+            {selectedTable === "Payload" && <EquipmentTable data={equipmentData} type="payload" />}
+            {selectedTable === "Geodetic GPS" && <EquipmentTable data={equipmentData} type="gps" />}
+            {selectedTable === "Laptop" && <EquipmentTable data={equipmentData} type="laptop" />}
+            {selectedTable === "Battery" && <EquipmentTable data={equipmentData} type="battery" />}
+            {selectedTable === "Other" && <EquipmentTable data={equipmentData} type="other" />}
           </div>
         </div>
       </div>
@@ -42,193 +61,44 @@ function EquipmentCompany() {
   );
 }
 
-function UAVTable() {
-    const UAVData = [
-      {
-        id: 1,
-        UAVtype: '',
-        UAVid: '',
-        status: 'available',
-      },
-      {
-        id: 2,
-        UAVtype: '',
-        UAVid: '',
-        status: 'not available',
-      },
-      {
-        id: 3,
-        UAVtype: '',
-        UAVid: '',
-        status: 'available',
-      },
-      {
-        id: 4,
-        UAVtype: '',
-        UAVid: '',
-        status: 'not availabke',
-      },
-     
-    ];
-    return (
-      <table className="table table-hover text-center table-bordered" style={{ borderColor: '#143893' }}>
-        <thead style={{ width: "100%"}} >
-          <tr>
-            <th scope="col" style={{ width: "5%", backgroundColor: '#143893', color: '#CCE6FF' }}>
-              #
-            </th>
-            <th scope="col" style={{ width: "40%", backgroundColor: '#143893', color: '#CCE6FF' }}>
-              UAV Type
-            </th>
-            <th scope="col" style={{ width: "40%", backgroundColor: '#143893', color: '#CCE6FF' }}>
-              UAV id
-            </th>
-            <th scope="col" style={{ width: "15%", backgroundColor: '#143893', color: '#CCE6FF'}}>
-              status
-            </th>
+function EquipmentTable({ data, type }) {
+  const typeMap = {
+    drone: { typeField: "drone_type", idField: "drone_id" },
+    payload: { typeField: "payload_type", idField: "payload_id" },
+    gps: { typeField: "gps_type", idField: "gps_id" },
+    laptop: { typeField: "laptop_type", idField: "laptop_id" },
+    battery: { typeField: "battery_type", idField: "battery_id" },
+    other: { typeField: "other_type", idField: "other_id" }
+  };
+
+  const filteredData = data.filter(item => item[typeMap[type].typeField] && item[typeMap[type].idField]);
+
+  return (
+    <table className="table table-hover text-center table-bordered" style={{ borderColor: '#143893' }}>
+      <thead>
+        <tr>
+          <th style={{ width: "5%", backgroundColor: '#143893', color: '#CCE6FF' }}>#</th>
+          <th style={{ width: "40%", backgroundColor: '#143893', color: '#CCE6FF' }}>{type.toUpperCase()} Type</th>
+          <th style={{ width: "40%", backgroundColor: '#143893', color: '#CCE6FF' }}>{type.toUpperCase()} ID</th>
+          <th style={{ width: "15%", backgroundColor: '#143893', color: '#CCE6FF' }}>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredData.map((item, index) => (
+          <tr key={index}>
+            <th scope="row">{index + 1}</th>
+            <td>{item[typeMap[type].typeField]}</td>
+            <td>{item[typeMap[type].idField]}</td>
+            <td>
+              <button type="button" className="btn btn-primary">
+                <i className="bi bi-pencil-square"></i>
+              </button>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {UAVData.map((data) => (
-            <tr>
-              <th scope="row">{data.id}</th>
-              <td>{data.UAVtype}</td>
-              <td>{data.UAVid}</td>
-              <td>
-              {data.status === 'available' ? 
-                    <button type="button" class="btn btn-success"><i class="bi bi-check2"></i></button>
-                    : <button type="button" class="btn btn-danger"><i class="bi bi-x"></i></button>
-                  }
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-  
-  function PayloadTable() {
-      const PayloadData = [
-          {
-            id: 1,
-            payloadType: '',
-            payloadID: '',
-            status: 'available',
-          },
-          {
-            id: 2,
-            payloadType: '',
-            payloadID: '',
-            status: 'not available',
-          },
-          {
-            id: 3,
-            payloadType: '',
-            payloadID: '',
-            status: '',
-          },
-        ];
-        return (
-          <table className="table table-hover text-center">
-            <thead style={{ width: "100%"}} >
-              <tr>
-                <th scope="col" style={{ width: "5%", backgroundColor: '#143893', color: '#CCE6FF'}}>
-                  #
-                </th>
-                <th scope="col" style={{ width: "40%", backgroundColor: '#143893', color: '#CCE6FF' }}>
-                  Payload Type
-                </th>
-                <th scope="col" style={{ width: "40%", backgroundColor: '#143893', color: '#CCE6FF' }}>
-                  Payload ID
-                </th>
-                <th scope="col" style={{ width: "15%", backgroundColor: '#143893', color: '#CCE6FF'}}>
-                  Status
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {PayloadData.map((data) => (
-                <tr>
-                  <th scope="row">{data.id}</th>
-                  <td>{data.payloadType}</td>
-                  <td>{data.ID}</td>
-                  <td>
-                  {data.status === 'available' ? 
-                    <button type="button" class="btn btn-success"><i class="bi bi-check2"></i></button>
-                    : <button type="button" class="btn btn-danger"><i class="bi bi-x"></i></button>
-                  }
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        );
-  }
-  
-  function GeodeticGPSTable() {
-      const GeodeticGPSData = [
-          {
-            id: 1,
-            GPStype: '',
-            GPSid: '',
-            status: 'available'
-          },
-          {
-            id: 2,
-            GPStype: '',
-            GPSid: '',
-            status: 'not available'
-          },
-          {
-            id: 3,
-            GPStype: '',
-            GPSid: '',
-            status: 'not available'
-          },
-          {
-            id: 4,
-            GPStype: '',
-            GPSid: '',
-            status: 'available'
-          },
-          
-          
-        ];
-        return (
-          <table className="table table-hover text-center">
-            <thead style={{ width: "100%"}} >
-              <tr>
-                <th scope="col" style={{ width: "5%", backgroundColor: '#143893', color: '#CCE6FF'}}>
-                  #
-                </th>
-                <th scope="col" style={{ width: "40%", backgroundColor: '#143893', color: '#CCE6FF' }}>
-                  GPS Type
-                </th>
-                <th scope="col" style={{ width: "40%", backgroundColor: '#143893', color: '#CCE6FF' }}>
-                  GPS ID
-                </th>
-                <th scope="col" style={{ width: "15%", backgroundColor: '#143893', color: '#CCE6FF'}}>
-                  Status
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {GeodeticGPSData.map((data) => (
-                <tr>
-                  <th scope="row">{data.id}</th>
-                  <td>{data.GPStype}</td>
-                  <td>{data.GPSid}</td>
-                  <td>
-                  {data.status === 'available' ? 
-                    <button type="button" class="btn btn-success"><i class="bi bi-check2"></i></button>
-                    : <button type="button" class="btn btn-danger"><i class="bi bi-x"></i></button>
-                  }
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        );
-  }
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default EquipmentCompany;
