aaaa5e763b94f7e011458999d4166fa169795aa0
Developmenttdid <development@terra-drone.co.id>
2025-05-26 11:18:41 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,6 +30,12 @@ import OperationManualCompany from './pages/Company/OperationManual';
 import SOP from './pages/Company/SOP';
 import PrivateRoutes from "./utils/PrivateRoutes";
 import { AuthProvider } from "./utils/AuthProvider";
+import Equipment_db from './pages/Project_Config_Db/equipment_db';
+import LegalDocument_db from './pages/Project_Config_Db/legaldoc_db';
+import Personnel_db from './pages/Project_Config_Db/personnel_db';
+import ProjectStatus_db from './pages/Project_Config_Db/projectstatus_db';
+import ProjectTimeline_db from './pages/Project_Config_Db/projecttimeline_db';
+import  ConfigNav_db from './components/confignav_db';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -42,7 +48,6 @@ root.render(
           {/* Public Route */}
           <Route path="/" element={<App />} />
           <Route path="/ForgotPassword" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
 
           {/* 🔥 Wrap ALL protected routes inside PrivateRoutes */}
           <Route element={<PrivateRoutes />}>
@@ -55,6 +60,14 @@ root.render(
                 <Route path="/Project/Personnel" element={<Personnel />} />
                 <Route path="/Project/Equipment" element={<Equipment />} />
                 <Route path="/Project/LegalDocument" element={<LegalDocument />} />
+              </Route>
+
+              <Route element={<NavconfigLayout_db />}>
+                <Route path="/Project/ProjectStatus_db" element={<ProjectStatus_db />} />
+                <Route path="/Project/ProjectTimeline_db" element={<ProjectTimeline_db />} />
+                <Route path="/Project/Personnel_db" element={<Personnel_db />} />
+                <Route path="/Project/Equipment_db" element={<Equipment_db />} />
+                <Route path="/Project/LegalDocument_db" element={<LegalDocument_db />} />
               </Route>
 
               <Route path="/Company" element={<Company />} />
@@ -110,6 +123,17 @@ function NavconfigLayout() {
   return (
     <div style={{ display: "flex" }}>
       <ConfigNav />
+      <div style={{ flex: 1, padding: "20px" }}>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+function NavconfigLayout_db() {
+  return (
+    <div style={{ display: "flex" }}>
+      <ConfigNav_db />
       <div style={{ flex: 1, padding: "20px" }}>
         <Outlet />
       </div>
